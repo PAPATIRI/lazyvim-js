c5e152dfcb1832892f6406ffd70e0b089d34f7f0
PAPATIRI <syariftaufik11@gmail.com>
2024-08-05 10:22:25 +0700

    setup neovim dengan lazyVim untuk koding di ekosistem reactjs

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,26 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+-- quit insert mode
+map("i", "jj", "<Esc>", { desc = "quit insert mode" })
+map("i", "jk", "<Esc>", { desc = "quit insert mode" })
+
+-- Set default terminal to PowerShell
+require("lazyvim.util.terminal").setup("pwsh")
+
+-- floating terminal
+local lazyterm = function()
+  LazyVim.terminal(nil, {
+    cwd = LazyVim.root(),
+    size = { width = 0.7, height = 0.7 },
+  })
+end
+
+map("n", "<leader>ft", lazyterm, { desc = "Terminal (Root Dir)" })
+map("n", "<leader>fT", function()
+  LazyVim.terminal()
+end, { desc = "Terminal (cwd)" })
+map("n", "<c-/>", lazyterm, { desc = "Terminal (Root Dir)" })
+map("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
